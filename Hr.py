05485c63c0d688e1301c3f50e8569d60da3a0c17
swapnakonda <swaruswapna331@gmail.com>
2021-07-18 16:21:10 +0530

    more info of managers

--- Hr.py
+++ Hr.py
@@ -5,14 +5,16 @@ class Hr:
         print("in no argument constructor")
 
     def get_hr_emprequested_info(self):
-        emp1 = Employee(10,"sop",10000)
+        emp1 = Employee(10,"sop",10000,["raghu","arun"])
         return emp1.get_emp_info()
 
 
 hr = Hr()
-empno, empname, salary, anualsal, deptno = hr.get_hr_emprequested_info()
+empno, empname, salary, first_manager,second_manager, anualsal, deptno = hr.get_hr_emprequested_info()
 print('empno - ',empno)
 print('empnoame - ',empname)
-print('salary - ',salary)
+print('salary - ', salary)
+print('first_manager - ',first_manager)
+print('second_manager -', second_manager)
 print('amnualsal - ',anualsal)
 print('deptno - ',deptno)
--- employe.py
+++ employe.py
@@ -2,15 +2,16 @@
 
 class Employee():
 
-    def __init__(self, empno, empname, salary):
+    def __init__(self, empno: int, empname: str, salary: float, managers: list):
         print("employee constructor")
         self.empno = empno
         self.empname = empname
         self.salary = salary
+        self.managers = managers
 
     def get_emp_info(self):
         print("in emp info")
-        return self.empno, self.empname, self.salary, self.get_emp_annual_salary(), self.emp_department_nor()
+        return self.empno, self.empname, self.salary,self.managers[0],self.managers[1], self.get_emp_annual_salary(), self.emp_department_nor()
 
     def get_emp_annual_salary(self):
         print('in emp anual salary')
